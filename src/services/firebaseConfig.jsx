c1a0843aee0ccb236d7b70c9697c0b0e9a98e4ee
lucasccgomes <lucasccgomes@protonmail.com>
2024-08-01 13:00:44 -0300

    feat(firebase): add firebase configuration and authentication setup

---
+++ src/services/firebaseConfig.jsx
@@ -0,0 +1,21 @@
+// src/firebaseConfig.js
+import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider, setPersistence, browserLocalPersistence } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDFlHqVeVRxJnQjsxBPpr3I3ihFyQ_J-1g",
+  authDomain: "familyfinanceappweb.firebaseapp.com",
+  projectId: "familyfinanceappweb",
+  storageBucket: "familyfinanceappweb.appspot.com",
+  messagingSenderId: "842229967094",
+  appId: "1:842229967094:web:e27fedb0ca900d7e19e4d7",
+  measurementId: "G-D4NY0WQ5QS"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+setPersistence(auth, browserLocalPersistence);
+
+export { auth, provider };
