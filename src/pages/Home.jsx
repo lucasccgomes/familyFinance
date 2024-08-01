a221d8ecbd989fe1a9120d8a96cd9dea2cb40781
lucasccgomes <lucasccgomes@protonmail.com>
2024-08-01 13:01:06 -0300

    feat(home): create home page component

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+// src/pages/Home.js
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-2xl">Bem-vindo à Home!</h1>
+    </div>
+  );
+};
+
+export default Home;
