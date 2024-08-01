2c50cf3961e07de6f0982c74f26e9e52cf837cdf
lucasccgomes <lucasccgomes@protonmail.com>
2024-08-01 13:01:20 -0300

    feat(router): add authentication check and route protection

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+// src/App.js
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Login from './pages/Login';
+import Home from './pages/Home';
+import { auth } from './services/firebaseConfig';
+import { onAuthStateChanged } from 'firebase/auth';
+
+const App = () => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <Router>
+      <Routes>
+        <Route 
+          path="/login" 
+          element={user ? <Navigate to="/home" /> : <Login />} 
+        />
+        <Route 
+          path="/home" 
+          element={user ? <Home /> : <Navigate to="/login" />} 
+        />
+        <Route path="*" element={<Navigate to={user ? "/home" : "/login"} />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
