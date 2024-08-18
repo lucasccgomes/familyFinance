2608226e8918dd48e308694198ee41565dc48ba4
Lucasccgomes <lucasccgomes@protonmail.com>
2024-08-18 10:01:47 -0300

    config

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.1039sj6tng8"
+    "revision": "0.v9e1a8220eo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
